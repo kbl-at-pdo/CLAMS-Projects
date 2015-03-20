cb5045ea0e67aba74d74693791d4036d73250633
kbl-at-pdo <kbleclair@partners.org>
2015-03-20 15:35:33 -0400

    Dad's Updates to Anal's microarray analysis

--- R/Anal Desai Microarray Analysis.R
+++ R/Anal Desai Microarray Analysis.R
@@ -1,19 +1,18 @@
 data.o <- read.csv("Data/Anal Desai/CCL4 Microarray IPA.csv")
-data.x <- data.o[data.o$LOG.MT.1>0,]
-write.csv(data.x,"Data/Anal Desai/CCL4 Microarray IPA x.csv")
 
-data.y <- data.o[mean(
-  data.o$LOG.MT.1) - 3.0 * sd(data.o$LOG.MT.1) < data.o$LOG.MT.1 &
-    data.o$LOG.MT.1 < mean(data.o$LOG.MT.1) + 3.0 * sd(data.o$LOG.MT.1),
-  ]
+data.o$NEW.WT.MEAN <- apply(data.o[,c("LOG2.WT1", "LOG.WT.2", "LOG.WT.3")],1,mean) 
+data.o$WT.SD <- apply(data.o[,c("LOG2.WT1", "LOG.WT.2", "LOG.WT.3")],1,sd) 
 
-write.csv(data.y,"Data/Anal Desai/CCL4 Microarray IPA y.csv")
+data.o$LOG2.WT1.VALID <- 1
+
+data.o$LOG2.WT1.VALID[
+  (data.o$LOG2.WT1 > (data.o$NEW.WT.MEAN) + 2.0 * data.o$WT.SD | 
+     data.o$LOG2.WT1 < (data.o$NEW.WT.MEAN - 2.0 * data.o$WT.SD))] <- 0
 
 
-## All WT <- "C2Plus_1771B", "C3Plus_1771D", "C4Plus_1771F"
-
-x <- c(data.o$LOG2.WT1[1], data.o$LOG.WT.2[1], data.o$LOG.WT.3[1])
-
-mean.x <- mean(x)
-
-mean(x) - 
+                              
+                              
+                              
+                              
+  
+  
